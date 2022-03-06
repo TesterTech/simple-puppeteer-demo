eb168979ff6091f748b0612c31d0da86d5ff8336
Eric de Graaf <egraaf@thinkpad.local>
2022-03-06 14:44:19 +0100

    initial commit

---
+++ example.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch(
+    {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless : false
+      }
+  );
+  const page = await browser.newPage();
+
+  process.on('unhandledRejection', (reason, p) => {
+    console.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
+    browser.close();
+  });
+
+  await page.goto('https://google.com', {
+    waitUntil: 'domcontentloaded',
+  });
+
+  var output = await page.evaluate(() => {
+    return;
+  });
+
+  await browser.close();
+})();
