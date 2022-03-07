815e0423c7fcad36a904e75bf06ed84163cc4731
Eric de Graaf <egraaf@thinkpad.local>
2022-03-07 20:06:49 +0100

    add some comments to the demo

--- example.js
+++ example.js
@@ -5,8 +5,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch(
     {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        headless : false
+        args: ['--no-sandbox', '--disable-setuid-sandbox'], //https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
+        headless : false //change to false if running in pipeline (default is also false, btw)
       }
   );
   const page = await browser.newPage();
@@ -19,6 +19,8 @@ const puppeteer = require('puppeteer');
   await page.goto('https://google.com', {
     waitUntil: 'domcontentloaded',
   });
+  const hrefElement = await page.$('asfffda'); // clicking any link for the demo
+  await hrefElement.click();
 
   var output = await page.evaluate(() => {
     return;
